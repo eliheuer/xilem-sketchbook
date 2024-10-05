780c7a64978e6063a261fc383a9d945e32f32b72
Eli Heuer <elih@protonmail.com>
2024-10-04 23:57:52 -0700

    hello world

---
+++ examples/hello_world.rs
@@ -0,0 +1,23 @@
+use xilem::{
+    view::{button, flex, label, Axis},
+    EventLoop, WidgetView, Xilem,
+};
+
+struct AppState {
+    count: i32,
+}
+
+fn app_logic(state: &mut AppState) -> impl WidgetView<AppState> {
+    flex((
+        button("-", |state: &mut AppState| state.count -= 1),
+        label(format!("Count: {}", state.count)),
+        button("+", |state: &mut AppState| state.count += 1),
+    ))
+    .direction(Axis::Horizontal)
+}
+
+fn main() {
+    let app = Xilem::new(AppState { count: 0 }, app_logic);
+    app.run_windowed(EventLoop::with_user_event(), "My Xilem App".into())
+        .unwrap();
+}
